438cc2223f79b8954d7aba6432a07691d345a47a
fernando-demingo <33869747+fernando-demingo@users.noreply.github.com>
2018-03-19 21:55:01 +0100

    Add files via upload

--- Random Rule Application/RandomDerivation.R
+++ Random Rule Application/RandomDerivation.R
@@ -11,15 +11,15 @@ procressGrammarRandom <- function(input_file, max_iter) {
   derivation_list <- list()
   repeat {
     resultado <- iterate_rule(expression, g)
+    if (resultado[[2]] == FALSE)
+      rules_applications <- rules_applications + 1 
     derivation_list <- append(derivation_list, resultado[[4]])
-    rules_applications <- rules_applications + 1 
     expression <- resultado[[1]]
-    if ((resultado[[2]] == TRUE) || rules_applications > max_iter) {
+    if ((resultado[[2]] == TRUE) || rules_applications >= max_iter) {
       solution <- resultado[[3]]
       break
-    }
+    } 
     expression_list <- append(expression_list, paste(unlist(expression), collapse=" "))
   }   
-  list(resultado=resultado, rules_applications=rules_applications, expression_list=expression_list, derivation_list=derivation_list)
+  list(resultado=resultado, rules_applications=rules_applications, expression_list=expression_list, derivation_list=derivation_list, expression=unlist(expression))
 }
-
